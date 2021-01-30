31ead8261950f8e18bb669ee01124a19d4167073
r-spiel <richelle.mehlhaff@gmail.com>
2021-01-29 18:56:09 -0800

    make many to one relationship from userPlant, post method working

--- calendar/src/main/java/com/calendarapi/calendar/userPlant/UserPlant.java
+++ calendar/src/main/java/com/calendarapi/calendar/userPlant/UserPlant.java
@@ -1,6 +1,9 @@
 package com.calendarapi.calendar.userPlant;
 
 import com.calendarapi.calendar.plant.Plant;
+import com.calendarapi.calendar.user.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.data.util.Lazy;
 
 import javax.persistence.*;
 
@@ -28,6 +31,11 @@ public class UserPlant {
     private Integer lifespan;
     private Integer harvest;
     private String sunRequirement;
+
+    @JsonIgnore // prevents from serializing user
+    @ManyToOne(fetch= FetchType.LAZY)
+    @JoinColumn(name="user_fid", nullable = false)
+    private User user;
     // add attribute for holding array of events
 
     public UserPlant () {
@@ -108,6 +116,11 @@ public class UserPlant {
         this.sunRequirement = sunRequirement;
     }
 
+    public User getUser() {
+        return user;
+    }
 
-
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
--- calendar/src/main/java/com/calendarapi/calendar/userPlant/UserPlantService.java
+++ calendar/src/main/java/com/calendarapi/calendar/userPlant/UserPlantService.java
@@ -1,5 +1,7 @@
 package com.calendarapi.calendar.userPlant;
 
+import com.calendarapi.calendar.user.User;
+import com.calendarapi.calendar.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,10 +13,12 @@ import java.util.Optional;
 public class UserPlantService {
 
     private final UserPlantRepository userPlantRepository;
+    private final UserService userService;
 
     @Autowired
-    public UserPlantService(UserPlantRepository userPlantRepository) {
+    public UserPlantService(UserPlantRepository userPlantRepository, UserService userService) {
         this.userPlantRepository = userPlantRepository;
+        this.userService = userService;
     }
 
     @GetMapping
@@ -22,12 +26,14 @@ public class UserPlantService {
         return userPlantRepository.findAll();
     }
 
-    public void addNewUserPlant(UserPlant userPlant) {
+    public void addNewUserPlant(UserPlant userPlant, Long userId) {
         // name must be unique
         Optional<UserPlant> userPlantByName = userPlantRepository.findUserPlantByName(userPlant.getName());
         if (userPlantByName.isPresent()) {
             throw new IllegalStateException("plant name already taken");
         }
+        User user = userService.showUser(userId);
+        userPlant.setUser(user);
         userPlantRepository.save(userPlant);
     }
 }
