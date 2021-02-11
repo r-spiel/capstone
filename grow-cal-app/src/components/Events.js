9725cb4e184add1931b3032e41bfbe8ee958fb76
r-spiel <richelle.mehlhaff@gmail.com>
2021-02-11 12:22:25 -0800

    update display of event date/time

--- grow-cal-app/src/components/Events.js
+++ grow-cal-app/src/components/Events.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import PropTypes from 'prop-types';
 import Event from './Event'
 import localStorage from 'local-storage';
+import moment from 'moment'
 
 const Events = ({eventList}) => {
 
@@ -10,17 +11,15 @@ const Events = ({eventList}) => {
 
 
     return (
-      <div>
-        <Event singleEvent={singleEvent} />
+      <div className="card" key={singleEvent.id}>
+        {console.log(singleEvent.startTime)}
+        {console.log(typeof singleEvent.startTime)}
+        {console.log(typeof singleEvent.startTime)}
+        <p>Event title: {singleEvent.title}</p>
+        <p>Start Date/Time: {moment(singleEvent.startTime).format('MMMM Do YYYY, h:mm a')}</p>
+        {console.log(moment(singleEvent.startTime).format('MMMM Do YYYY, h:mm:ss a'))}
+        <p>End Date/Time: {moment(singleEvent.endTime).format('MMMM Do YYYY, h:mm a')}</p>
       </div>
-      // <div className="card" key={singleEvent.id}>
-      //   {/* {console.log(singleEvent.startTime)}
-      //   {console.log(typeof singleEvent.startTime)}
-      //   {console.log(typeof singleEvent.startTime)} */}
-      //   <p>Event title: {singleEvent.title}</p>
-      //   <p>Start Date/Time: {dateStartReadable}</p>
-      //   <p>End Date/Time: {singleEvent.endTime}</p>
-      // </div>
     )
   })
 
