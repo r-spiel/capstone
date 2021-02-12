8ef1ceb30fe7cc7747011239a6708431d78ae23c
r-spiel <richelle.mehlhaff@gmail.com>
2021-02-11 21:28:50 -0800

    delete console logs

--- grow-cal-app/src/components/Events.js
+++ grow-cal-app/src/components/Events.js
@@ -12,12 +12,8 @@ const Events = ({eventList}) => {
 
     return (
       <div className="card" key={singleEvent.id}>
-        {console.log(singleEvent.startTime)}
-        {console.log(typeof singleEvent.startTime)}
-        {console.log(typeof singleEvent.startTime)}
         <p>Event title: {singleEvent.title}</p>
         <p>Start Date/Time: {moment(singleEvent.startTime).format('MMMM Do YYYY, h:mm a')}</p>
-        {console.log(moment(singleEvent.startTime).format('MMMM Do YYYY, h:mm:ss a'))}
         <p>End Date/Time: {moment(singleEvent.endTime).format('MMMM Do YYYY, h:mm a')}</p>
       </div>
     )
