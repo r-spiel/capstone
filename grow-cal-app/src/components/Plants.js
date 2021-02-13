34cde82bb6c07a36b53197995999ad88bffafff0
r-spiel <richelle.mehlhaff@gmail.com>
2021-02-13 14:02:57 -0800

    working carousel

--- grow-cal-app/src/components/Plants.js
+++ grow-cal-app/src/components/Plants.js
@@ -5,33 +5,74 @@ import PlantDetails from './PlantDetails'
 import localStorage from 'local-storage';
 
 const Plants = ({plants, selectAPlant, unselectAPlant, selectedPlantId}) => {
-
+  
 
   const plantIconComponents = plants.map((plant) => {
     return (
-      <PlantIcon
-        key={plant.id} 
-        id={plant.id}
-        name={plant.name}
-        imageUrl={plant.imageUrl}
+      <div className="col-xs-3">
+        <PlantIcon
+          key={plant.id} 
+          id={plant.id}
+          name={plant.name}
+          imageUrl={plant.imageUrl}
 
-        selectedPlantId={selectedPlantId}
-        selectAPlant={selectAPlant}
-        unselectAPlant={unselectAPlant}
-      />
+          selectedPlantId={selectedPlantId}
+          selectAPlant={selectAPlant}
+          unselectAPlant={unselectAPlant}
+        />
+      </div>
+    )
+  })
+
+  const iconCarouselBuilder = () => {
+    let carouselMap = []
+    let i = 0
+    while (i < plants.length) {
+      if (i + 3 < plants.length) {
+        carouselMap.push(plantIconComponents.slice(i, i + 4))
+      } else { 
+        carouselMap.push(plantIconComponents.slice(i, plants.length))
+      }
+      
+      i += 4
+    }
+    return carouselMap
+  }
+
+  const carouselGroups = iconCarouselBuilder()
+
+  const plantInnerCarousel = carouselGroups.map(function(group, index) {
+    return (
+      <div className={index === 0 ? "carousel-item active": "carousel-item"}>
+        <div className="row justify-content-center">
+          {group}
+        </div>
+
+      </div>
+
     )
   })
 
 
-
   return (
     <div>
-      {/* <button onClick={clearSelectedPlantList} >Clear Selected Plants</button> */}
-      <p>Select a plant to view it's details and see more options:</p>
-      <span className="row m-5">
-        {plantIconComponents}
-      </span>
-      {/* { selectedPlants.length > 0 ? plantDetailsComponents : null } */}
+      <p>You have {plants.length} plants in your list.</p>
+      <p>Select a plant from the carousel to view it's details and see more options:</p>
+      <div className="col-xs-12">
+        <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+          <div className="carousel-inner">
+            {plantInnerCarousel}
+          </div>
+          <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="sr-only">Previous</span>
+          </a>
+          <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="sr-only">Next</span>
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
