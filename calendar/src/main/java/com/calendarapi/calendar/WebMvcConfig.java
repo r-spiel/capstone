1b5d26374f74f95bacd8a54d8c7549fb26d38fbf
r-spiel <richelle.mehlhaff@gmail.com>
2021-02-15 11:54:34 -0800

    update calendar api webMvcConfig

--- calendar/src/main/java/com/calendarapi/calendar/WebMvcConfig.java
+++ calendar/src/main/java/com/calendarapi/calendar/WebMvcConfig.java
@@ -11,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://r-spiel.github.io/capstone/")
+                .allowedOrigins("http://localhost:3000", "https://r-spiel.github.io")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
